cb2eafaf3e7c6c76de3b77ee90fa13cc13b486f3
JGGrande <joaogabriel7303@gmail.com>
2024-11-21 11:08:59 -0300

    Adiciona construtor para a classe de visita

--- src/modules/visit/domain/entities/visit.entity.ts
+++ src/modules/visit/domain/entities/visit.entity.ts
@@ -2,6 +2,21 @@ import { Deposit } from "../types/deposit.type";
 import { Sample } from "../types/sample.type";
 import { Treatment } from "../types/treatment.type";
 
+type VisitProps = {
+  id: number;
+  activity: string;
+  type: string;
+  pending: string | null;
+  startedAt: Date;
+  endedAt: Date;
+  inspected: boolean;
+  deposit: Deposit;
+  sample: Sample | null;
+  treatment: Treatment | null;
+  userId: number;
+  residenceId: number;
+};
+
 export class Visit {
   readonly id: number;
   activity: string;
@@ -15,4 +30,19 @@ export class Visit {
   treatment: Treatment | null;
   userId: number;
   residenceId: number;
+
+  constructor(props: VisitProps){
+    this.id = props.id;
+    this.activity = props.activity;
+    this.type = props.type;
+    this.pending = props.pending;
+    this.startedAt = props.startedAt;
+    this.endedAt = props.endedAt;
+    this.inspected = props.inspected;
+    this.deposit = props.deposit;
+    this.sample = props.sample;
+    this.treatment = props.treatment;
+    this.userId = props.userId;
+    this.residenceId = props.residenceId;
+  }
 }
